2ef48dcffe5a70cf841c2e53493d3981905a8fa6
Denis Leemann <denis.leemann@camptocamp.com>
2025-01-08 13:32:39 +0100

    [Clean]: remove unused fund source

--- update_data.py
+++ update_data.py
@@ -4,9 +4,6 @@ funds = {
     'pf-swiss_small_caps': 'https://www.swissfunddata.ch/sfdpub/fr/funds/excelData/102619',
     'pf-high_div_a': 'https://www.swissfunddata.ch/sfdpub/fr/funds/excelData/85672',
     'pf-pension100': 'https://www.swissfunddata.ch/sfdpub/fr/funds/excelData/114269',
-    'pf-pension75': 'https://www.swissfunddata.ch/sfdpub/fr/funds/excelData/64875',
-    'pf-pension45': 'https://www.swissfunddata.ch/sfdpub/fr/funds/excelData/7574',
-    'pf-pension25': 'https://www.swissfunddata.ch/sfdpub/fr/funds/excelData/10833',
 }
 cols = [
     'Date',
